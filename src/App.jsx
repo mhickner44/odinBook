155d6ad065c0258b7016dbf1a8bf93faa51d5146
Michael <rmicu1903@gmail.com>
2024-02-09 16:57:23 -0500

    login request working

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,13 @@ import { Link } from "react-router-dom";
 
 function App() {
 
-  return (
-
-    <>
+  return (    <>
       <div className='header'>
         <h1 id='title'>ODIN BOOk</h1>
         <ul>
           <li> <Link to="/login">login</Link></li>
         </ul>
       </div>
-
-
-
     </>
   )
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,37 @@
 import { useState } from 'react'
 
 function UserForm({ form }) {
-  const [formType, setForm] = useState(false);
 
+  const [formType, setForm] = useState(false);
   
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    const username = event.target.elements.username.value;
+    const password = event.target.elements.password.value;
+
+    const formData = new FormData(event.target)
+    const data = Object.fromEntries(formData)
+    console.log((data))
+    
+    const answer=await fetch('http://localhost:3000/login/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(
+        data
+      )
+    }).then(function(response) {
+      // The response is a Response instance.
+      // You parse the data into a useable format using `.json()`
+      return response.json();
+    }).then(function(data) {
+      // `data` is the parsed version of the JSON returned from the above endpoint.
+      console.log(data);  // { "userId": 1, "id": 1, "title": "...", "body": "..." }
+      
+      //store the token here and store in local storage.
+    });
+  }
+ 
   function handleClick() {
     setForm(current => !current)
   }
@@ -11,25 +39,25 @@ function UserForm({ form }) {
   if (form == true) {
     return (
       // the login form 
-      <form className='signUp' action=''>
+      <form className='signUp' onSubmit={handleSubmit} >
         <label>Username:</label>
-        <input type="text" ></input>
+        <input type="text" name='username'></input>
         <label >Password:</label>
-        <input type='password' ></input>
+        <input type='password'  name='password'></input>
         <label >Password:</label>
-        <input type='password' ></input>
-        <button>Submit</button>
+        <input type='password'  ></input>
+        <button type="submit">Submit</button>
       </form>)
 
   } else {
     return (
       // the login form 
-      <form className='login' action=''>
+      <form className='login' onSubmit={handleSubmit}  >
         <label>Username:</label>
-        <input type="text" ></input>
+        <input type="text" name='username' ></input>
         <label >Password:</label>
-        <input type='password' ></input>
-        <button>Submit</button>
+        <input type='password'  name='password'></input>
+        <button type="submit">Submit</button>
       </form>)
   }
 }
