7891087f96f340a7f34a5393e266f5d19e2576ae
Michael <rmicu1903@gmail.com>
2024-04-15 22:22:45 -0400

    Revert "readme link"
    This reverts commit 07024e002cc6dd6ae25f00447e9b65202e997d33.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@
 
 import './index.css'
 import { Link } from "react-router-dom";
-
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 
 function App() {
